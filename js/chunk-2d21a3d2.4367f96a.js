212f65d24dd160bc96b5c435d5a9e7263a949c60
lleballex <lleballex@yandex.ru>
2020-08-19 20:33:39 +0300

    Make production version

---
+++ js/chunk-2d21a3d2.4367f96a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("Home page")])},u=[],c={name:"Home"},l=c,o=t("2877"),r=Object(o["a"])(l,a,u,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.4367f96a.js.map
---
+++ js/chunk-2d22d746.8e5cff31.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d22d746"],{f820:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},c=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("About page!")])])}],r={name:"About"},a=r,l=t("2877"),o=Object(l["a"])(a,u,c,!1,null,null,null);n["default"]=o.exports}}]);
+//# sourceMappingURL=chunk-2d22d746.8e5cff31.js.map

30ecdad50371ac078427428caf55a98da5dbcff0
lleballex <lleballex@yandex.ru>
2020-08-20 17:58:46 +0300

    Added ssl

---
+++ js/chunk-2d21a3d2.213b272e.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d21a3d2"],{bb51:function(e,n,t){"use strict";t.r(n);var u=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("h1",[e._v("Home page")])},a=[],o={name:"Home",created:function(){this.$router.push({name:"Posts"})}},s=o,c=t("2877"),r=Object(c["a"])(s,u,a,!1,null,null,null);n["default"]=r.exports}}]);
+//# sourceMappingURL=chunk-2d21a3d2.213b272e.js.map
